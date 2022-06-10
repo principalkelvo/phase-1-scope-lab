ccc958707cc18e476a063c65cf8375f4883caba1
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-06-10 13:56:37 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase()
+    return customerName;
+    // console.log("customerName", customerName);
+}
+// upperCaseCustomerName();
+
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+  return bestCustomer;
+  // console.log(bestCustomer);
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = "maybe bob";
+  return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'John';
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "Doe";
+  return leastFavoriteCustomer;
+}
